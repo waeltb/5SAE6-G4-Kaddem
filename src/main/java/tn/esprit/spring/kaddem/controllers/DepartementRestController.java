79164e13f94f6d9b2c3a9134d5ef8ded144ce50a
MohamedOuni <ounimed33@gmail.com>
2023-11-09 01:06:02 +0100

    testfrontTEST

--- src/main/java/tn/esprit/spring/kaddem/controllers/DepartementRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/DepartementRestController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/departement")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://192.168.3.44:8089")
 public class DepartementRestController {
 	IDepartementService departementService;
 	// http://localhost:8089/Kaddem/departement/retrieve-all-departements
